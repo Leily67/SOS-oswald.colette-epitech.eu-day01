09459f704b9b536051f20f2bb5b95ca1b42ee040
Leily <colette.oswald@epitech.eu>
2022-02-07 20:19:15 +0100

    exo supp piscine php

--- ex_09/doom.php
+++ ex_09/doom.php
@@ -2,16 +2,12 @@
 
 function draw_triangle($nb) {
    
-    $k = 2 * $nb - 2;
- 
-    for ($i = 0; $i < $nb; $i++)
-    {
-        for ($j = 0; $j < $k; $j++)
+    for ($i = 0; $i < $nb; $i++){
+        for ($j = 0; $j <= $nb-$i; $j++){
             echo " ";
-        $k = $k - 1;
-        for ($j = 0; $j <= $i; $j++ )
-        {
-            echo "* ";
+        }
+        for ($j =0; $j <= $i+$i; $j++){
+            echo "*";
         }
         echo "\n";
     }
