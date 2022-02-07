910b05ca2abc5d2a05e27d72d37318e25c1df7af
Leily <colette.oswald@epitech.eu>
2022-02-07 19:49:54 +0100

    exo supp piscine php

--- ex_03/coin.php
+++ ex_03/coin.php
@@ -1,7 +1,7 @@
 <?php
 
-function coin(){
-    for($i = 1; $i <= 10; $i++){
+function coin($nb){
+    for($i = 1; $i <= $nb; $i++){
         if ($i % 2 != 0) echo "COIINNN !!\n"; else echo "coin.\n";
     }
 }
